b8949a5c6f17b6b47bc35aface55c6bff8b487f8
StepanSobolev <190889Ssv>
2022-08-11 16:16:13 +0300

    while_for

---
+++ while_for.py
@@ -0,0 +1,15 @@
+changes = input('Введите число: ')
+print(changes)
+count = 1
+
+for i in range(1, int(changes)+1):
+    if i % 3 != 0:
+        print(i ** 3, end=', ')
+
+print()
+
+
+while count <= int(changes):
+    if count % 3 != 0:
+        print(count ** 3, end=', ')
+    count += 1
