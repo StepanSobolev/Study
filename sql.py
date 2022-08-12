7c2b7c0cda0b8ee0a9f51f6bf248629780eeab26
StepanSobolev <190889Ssv>
2022-08-12 16:38:22 +0300

    traning 12

--- sql.py
+++ sql.py
@@ -4,18 +4,22 @@ db = sqlite3.connect('base.db')
 cur = db.cursor()
 
 print('Wat do you want?')
-choice = input('Ad member tab N: \n see member tab S: \n update age memeber tab U:  ')
-cur.execute("""CREATE TABLE IF NOT EXISTS articles(
-        name text PRIMARY KEY,
-        age integer
-    )""")
+choice = input('Ad member tab N: \nSee member tab S: \nUpdate age memeber tab U: \nDelete user tab D: ')
 
+# cur.execute("""CREATE TABLE IF NOT EXISTS articles(
+#         name text PRIMARY KEY,
+#         age text
+#     )""")
 
 def new_member():
-    name = input('Введите имя: ')
-    age = int(input('Введите возраст: '))
-    cur.execute("""INSERT INTO articles VALUES (?, ?)""", (name, age))
-    db.commit()
+    try:
+        name = input('Введите имя: ')
+        age = int(input('Введите возраст: '))
+        cur.execute("""INSERT INTO articles VALUES (?, ?)""", (name, age))
+        db.commit()
+    except:
+        print('Такое имя уже есть попробуй другое')
+        new_member()
 
 
 def update_age():
@@ -26,14 +30,24 @@ def update_age():
 
 
 def see_member():
-    see_all = input('If you see all tap Y: ')
-    if see_all.lower() == 'u':
+    see_user = input('Если хотите увидеть всех нажмите Y. Если хотите увидеть отдельного персонажа нажмите О.')
+    if see_user.lower() == 'y':
         cur.execute("SELECT name FROM articles")
         one_result = cur.fetchall()
         for i in one_result:
             d = ''
-            f = d.join(i)
-            print(f, end=', ')
+            print(d.join(i), end=', ')
+    elif see_user.lower() == 'o':
+        name = input('Введите имя: ')
+        templates = ''
+        print(name, 'age is',
+              templates.join(cur.execute(f"""SELECT age FROM articles WHERE name = '{name}'""").fetchone()))
+
+
+def delete_user():
+    name = input('Введите имя: ')
+    cur.execute(f"""DELETE FROM articles WHERE name = '{name}'""")
+    db.commit()
 
 
 if choice.lower() == 'n':
@@ -42,5 +56,7 @@ elif choice.lower() == 's':
     see_member()
 elif choice.lower() == 'u':
     update_age()
+elif choice.lower() == 'd':
+    delete_user()
 
 db.close()
